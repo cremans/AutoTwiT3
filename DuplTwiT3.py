bc5eebee6a70d6fad6f14252bff2264be4231754
cremans <88071871+cremans@users.noreply.github.com>
2021-11-20 23:05:10 +0000

    Update DuplTwiT3.py

--- DuplTwiT3.py
+++ DuplTwiT3.py
@@ -280,5 +280,6 @@ def main(vodID):
         os.system('winrar.exe a -afrar -df -m5 -mt3 -ri15 -t -tk -ts -v14g "' + nom_vid +
                   '.rar" "' + nom_vid + '.mp4"')
 
-main("1152502978")
-main("1153582310")
+main("1156150324")
+main("1157021170")
+main("1159052521")
